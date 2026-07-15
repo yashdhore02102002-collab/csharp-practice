de1af4ad48b9ca556d592ce995c81e6fcbe978a2
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-15 21:18:22 +0530

    Add:<StarPatternPyramid>

---
+++ 01-loops/StarPatternPyramid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class StarPattern
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+

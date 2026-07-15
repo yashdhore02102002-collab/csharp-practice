6c1975449478e608ddc5de37b4d950fad7350bcb
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-15 20:56:12 +0530

    Program programs

---
+++ 01-loops/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using System;
+
+class PalindromeCheck
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int reverse = 0;
+        int temp = number;
+
+        while (temp != 0)
+        {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp = temp / 10;
+        }
+
+        if (number == reverse)
+        {
+            Console.WriteLine(number + " is a Palindrome");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a Palindrome");
+        }
+    }
+}

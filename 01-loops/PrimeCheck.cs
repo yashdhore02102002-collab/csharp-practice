b2c3bd2272cf2d55b81d9f3d292628a38439316c
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 20:37:51 +0530

    Add:<PrimrCheck>

---
+++ 01-loops/PrimeCheck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class PrimeCheck
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool isPrime = true;
+
+        if (number <= 1)
+        {
+            isPrime = false;
+        }
+        else
+        {
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine(number + " is Prime");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not Prime");
+        }
+    }
+}

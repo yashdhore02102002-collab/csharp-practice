9da890d230789de7fdd293693133bb70c41d3c85
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 20:32:53 +0530

    Add:<ReverseStarPatternPyramid>

---
+++ 01-loops/ReverseStarPatternPyramid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class ReversePyramid
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i + 1; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

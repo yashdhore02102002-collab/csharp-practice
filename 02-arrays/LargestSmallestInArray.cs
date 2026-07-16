2182ba5221815f18924302f24502bccb1d60da51
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 21:22:22 +0530

    Add:<LargestSmallestInArray>

---
+++ 02-arrays/LargestSmallestInArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class LargestSmallestInArray
+{
+    static void Main()
+    {
+        int[] numbers = { 12, 45, 3, 67, 29, 8 };
+
+        int largest = numbers[0];
+        int smallest = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > largest)
+            {
+                largest = numbers[i];
+            }
+
+            if (numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+        }
+
+        Console.WriteLine("Largest number: " + largest);
+        Console.WriteLine("Smallest number: " + smallest);
+    }
+}

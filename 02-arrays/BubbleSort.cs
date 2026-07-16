83def5498bcbef48d1b8b462d69ef1a22fda26d4
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 21:19:37 +0530

    Add:<BubbleSort>

---
+++ 02-arrays/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int[] numbers = { 45, 12, 67, 3, 29, 8 };
+        int n = numbers.Length;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - 1 - i; j++)
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    // Swap kar
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.Write("Sorted array: ");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}

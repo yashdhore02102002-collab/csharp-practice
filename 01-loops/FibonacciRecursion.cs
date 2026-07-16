7cbcf390c3b11df1842f072148859ad2a6ca373a
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 20:59:19 +0530

    Add:<FibonacciRecursion>

---
+++ 01-loops/FibonacciRecursion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class FibonacciRecursion
+{
+    static int Fibonacci(int n)
+    {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number of terms: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+}

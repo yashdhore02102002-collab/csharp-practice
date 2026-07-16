53db895995dd35440c564bdfb7c2351044c7d61b
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 20:43:25 +0530

    Add:<FactorialRecursion>

---
+++ 01-loops/FactorialRecursion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class FactorialRecursion
+{
+    static long Factorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        long result = Factorial(n);
+        Console.WriteLine("Factorial of " + n + " is: " + result);
+    }
+}

9ad116ad50e7d8c3ed404c7ecce56759335c7417
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-16 20:55:52 +0530

    Add:<FibonacciLoop>

---
+++ 01-loops/FibonacciLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class FibonacciLoop
+{
+    static void Main()
+    {
+        Console.Write("Enter number of terms: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int first = 0, second = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}

11009a3bd8b94575e30309e634ca36e2f5177f62
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-07-20 10:03:40 +0530

    Add:<TwoSum>

---
+++ 02-arrays/TwoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class VowelCount
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string lower = input.ToLower();
+        int count = 0;
+
+        for (int i = 0; i < lower.Length; i++)
+        {
+            char ch = lower[i];
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Total vowels: " + count);
+    }
+}

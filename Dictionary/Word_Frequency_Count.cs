1f60107b5124cf973d037867f6508eada3fd9908
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-08-30 16:01:12 +0530

    Add: <Word_Frequency_Count>

---
+++ Dictionary/Word_Frequency_Count.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the Scentence: ");
+        string scentence = Console.ReadLine().ToLower();
+
+        string[] words = scentence.Split(' ');
+        Dictionary<string, int> frequency = new Dictionary<string, int >();
+
+        foreach (string word in words)
+        {
+            if (frequency.ContainsKey(word))
+            {
+                frequency[word]++; // allready exisist word incriment
+
+            }
+            else
+            {
+                frequency[word] = 1; // For new word            }
+            }
+
+        }
+     Console.WriteLine("\n--- Word Frequency ---");
+     foreach(KeyValuePair<string, int> entery in frequency)
+        {
+            Console.WriteLine(entery.Key + "->" + entery.Value + "time(s)");
+
+        }
+    }
+}

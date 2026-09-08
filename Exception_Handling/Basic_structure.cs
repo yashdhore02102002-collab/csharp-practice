d710071053da2aa8f303189bdf5751f9859a0ccd
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-08 21:25:10 +0530

    Add:<Basic_structure>

---
+++ Exception_Handling/Basic_structure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter A Number: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered: " + num);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Invalid Input. Please Enter a valid Integer.");
+            Console.WriteLine("Details: " + ex.Message);
+        }
+    }
+}

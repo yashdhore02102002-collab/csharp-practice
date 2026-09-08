87024e3fa901a27e99c288482c22d5a7439aaf33
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-08 22:25:26 +0530

    Add:<Finally_Block>

---
+++ Exception_Handling/Finally_Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter First Number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            int b = int.Parse(Console.ReadLine());
+            int result = a/b;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Invalid Number Format");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Second number cannot be zero");
+        }
+        finally
+        {
+            Console.WriteLine("Program execution completed.");
+        }
+    }
+}

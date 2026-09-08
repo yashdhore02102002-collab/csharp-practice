958fb10e754eac2adc51af28b3e17e2841fdcb48
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-08 21:57:01 +0530

    Add:<Multiple_catch_Blocks>

---
+++ Exception_Handling/Multiple_catch_Blocks.cs
@@ -0,0 +1,33 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter Array Size: ");
+            int size = int.Parse(Console.ReadLine());
+
+            int[] number = new int [size];
+            Console.WriteLine("Enter Index to Access: ");
+            int index = int.Parse(Console.ReadLine());
+            Console.WriteLine("Value at index " + index + ":" + number[index]);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error:Number Input Invalid");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Error: Index Out of Range");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error: Number Too Large");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Unexpected Error:" + ex.Message);
+        }
+    }
+}

7c61b026f3739f468a42909ccfd1006da0d49ede
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-09 14:27:09 +0530

    Add:<Custom_Exception>

---
+++ Exception_Handling/Custom_Exception.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+        
+    }
+}
+class Student
+{
+    private int age;
+    public int Age
+    {
+        get{return age;}
+        set
+        {
+            if(value < 0 || value > 100)
+            throw new InvalidAgeException("Age must be Between 0 and 100");
+            age = value;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Student s = new Student();
+            Console.Write("Enter Age: ");
+            s.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Age is : " + s.Age);
+        }
+        catch(InvalidAgeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Input Format.");
+        }
+    }
+}

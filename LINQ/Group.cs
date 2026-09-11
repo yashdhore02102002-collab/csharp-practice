4a1bee0c34f8c4a687d10977bae2e860edca71ec
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-11 16:31:35 +0530

    Add:<Group>

---
+++ LINQ/Group.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string Name;
+    public double Marks;
+    public string City;
+    public Student (string name, double marks, string city)
+    {
+        Name = name;
+        Marks = marks;
+        City = city;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+     List<Student> students = new List<Student>();
+     
+        Console.Write("Enter The no of Student: ");
+        int n = int.Parse(Console.ReadLine());
+        for(int i =0 ; i < n ; i++)
+            {
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine().ToUpper();
+                Console.Write("Enter Student Mark: ");
+                double marks = double.Parse(Console.ReadLine());
+                Console.Write("Enter Student City: ");
+                string city = Console.ReadLine();
+                students.Add(new Student(name, marks, city));
+                Console.WriteLine("Student Add Successfully.");
+                Console.WriteLine("\n------------------------");
+            }
+        //Group by city
+        var groupCity = students.GroupBy(s => s.City);
+        foreach(var group in groupCity)
+        {
+            Console.WriteLine("\nCity:" + group.Key);
+            foreach(var s in group)
+            Console.WriteLine(" " + s.Name + "-" + s.Marks);
+        }
+    }
+   
+}

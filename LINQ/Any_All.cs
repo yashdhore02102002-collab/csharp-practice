4156f7ca3165591dacf5cf775704d86f450aa134
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-11 16:51:38 +0530

    Add:<Any_All>

---
+++ LINQ/Any_All.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string Name;
+    public double Marks;
+    public string City;
+    public Student (string name, double marks, string city)
+    {
+        Name = name;
+        Marks = marks;
+        City = city;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+     List<Student> students = new List<Student>();
+     
+        Console.Write("Enter The no of Student: ");
+        int n = int.Parse(Console.ReadLine());
+        for(int i =0 ; i < n ; i++)
+            {
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine().ToUpper();
+                Console.Write("Enter Student Mark: ");
+                double marks = double.Parse(Console.ReadLine());
+                Console.Write("Enter Student City: ");
+                string city = Console.ReadLine();
+                students.Add(new Student(name, marks, city));
+                Console.WriteLine("Student Add Successfully.");
+                Console.WriteLine("\n------------------------");
+            }
+        //Any/All condition match
+        bool anyFail = students.Any(s => s.Marks >= 35 );
+        Console.WriteLine("Is any student Fail?" + anyFail);
+
+        bool allPass = students.All(s => s.Marks >= 35);
+        Console.WriteLine("Passed Students: " + allPass);
+    }
+   
+}

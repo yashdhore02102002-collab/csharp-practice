187daff12a71fac84a60db4bc875fdf9a7c74a3a
yashdhore02102002-collab <yashdhore02102002@gmail.com>
2026-09-11 15:37:29 +0530

    Add:<Select>

---
+++ LINQ/Select.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Student
+{
+    public string Name;
+    public double Marks;
+    public string City;
+    public Student (string name, double marks, string city)
+    {
+        Name = name;
+        Marks = marks;
+        City = city;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+     List<Student> students = new List<Student>();
+     
+        Console.Write("Enter The no of Student: ");
+        int n = int.Parse(Console.ReadLine());
+        for(int i =0 ; i < n ; i++)
+            {
+                Console.Write("Enter Student Name: ");
+                string name = Console.ReadLine().ToUpper();
+                Console.Write("Enter Student Mark: ");
+                double marks = double.Parse(Console.ReadLine());
+                Console.Write("Enter Student City: ");
+                string city = Console.ReadLine();
+                students.Add(new Student(name, marks, city));
+                Console.WriteLine("Student Add Successfully.");
+                Console.WriteLine("\n------------------------");
+            }
+        Console.WriteLine("Display Students Name: ");
+        // 80 पेक्षा जास्त marks असलेले students
+        var names = students.Select(s => s.Name);
+
+        foreach (var name in names)
+        Console.WriteLine(name); 
+     
+    }
+   
+}
